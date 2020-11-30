87ce3de237a4d0f8a262f668c726a652cac6f315
R0st1slav <rostiabrslav@gmail.com>
2020-11-30 16:57:30 +1000

    some changes

--- Notification/views.py
+++ Notification/views.py
@@ -3,4 +3,9 @@ from .models import Device, TypeDevice
 
 def base(request):
     devices = Device.objects.all()
-    return render(request, 'Notification/base.html', {'devices': devices})
+    type_devices = TypeDevice.objects.all()
+    context = {
+        'devices': devices,
+        'type_devices': type_devices,
+    }
+    return render(request, 'base.html', context)
